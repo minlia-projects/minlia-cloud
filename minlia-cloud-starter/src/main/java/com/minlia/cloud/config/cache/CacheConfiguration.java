02ef6431c84b201da34f49c00f6946bf3fbf9a95
will <will@minlia.com>
2017-10-12 11:39:00 +0800

    Update Deployer

---
+++ minlia-cloud-starter/src/main/java/com/minlia/cloud/config/cache/CacheConfiguration.java
@@ -0,0 +1,14 @@
+//package com.minlia.cloud.config.cache;
+//
+//import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.context.annotation.AdviceMode;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * Created by will on 10/12/17.
+// */
+//@Configuration
+//
+//public class CacheConfiguration {
+//
+//}
