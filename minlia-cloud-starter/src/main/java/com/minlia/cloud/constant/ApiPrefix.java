3889afa5536a795eb3ac73400d433660c44e41b8
garen <garen@garen.local>
2018-11-19 21:53:51 +0800

    Update at 20181119215351 by dev/garen

--- minlia-cloud-starter/src/main/java/com/minlia/cloud/constant/ApiPrefix.java
+++ minlia-cloud-starter/src/main/java/com/minlia/cloud/constant/ApiPrefix.java
@@ -21,6 +21,29 @@ public class ApiPrefix {
     public static final String V9 = API + "v9/";
     public static final String V10 = API + "v10/";
 
+
+//    public static final String V1_OPEN = API + "v1/open/";
+//    public static final String V2_OPEN = API + "v2/open";
+//    public static final String V3_OPEN = API + "v3/open";
+//    public static final String V4_OPEN = API + "v4/open";
+//    public static final String V5_OPEN = API + "v5/open";
+//    public static final String V6_OPEN = API + "v6/open";
+//    public static final String V7_OPEN = API + "v7/open";
+//    public static final String V8_OPEN = API + "v8/open";
+//    public static final String V9_OPEN = API + "v9/open";
+//    public static final String V10_OPEN = API + "v10/open";
+//
+//    public static final String OPEN_V1 = API + "open/v1/";
+//    public static final String OPEN_V2 = API + "open/v2/";
+//    public static final String OPEN_V3 = API + "open/v3/";
+//    public static final String OPEN_V4 = API + "open/v4/";
+//    public static final String OPEN_V5 = API + "open/v5/";
+//    public static final String OPEN_V6 = API + "open/v6/";
+//    public static final String OPEN_V7 = API + "open/v7/";
+//    public static final String OPEN_V8 = API + "open/v8/";
+//    public static final String OPEN_V9 = API + "open/v9/";
+//    public static final String OPEN_V10 = API + "open/v10/";
+
     public ApiPrefix() {
         throw new AssertionError();
     }
