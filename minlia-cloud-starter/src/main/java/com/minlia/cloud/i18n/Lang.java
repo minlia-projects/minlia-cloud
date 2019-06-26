85bc7ddcaa1c658329c54d4f5f41ba0cc81b5ff6
garen <191285052@qq.com>
2019-06-26 20:01:28 +0800

    Update at 20190626200128 by dev/garen

--- minlia-cloud-starter/src/main/java/com/minlia/cloud/i18n/Lang.java
+++ minlia-cloud-starter/src/main/java/com/minlia/cloud/i18n/Lang.java
@@ -1,6 +1,7 @@
 package com.minlia.cloud.i18n;
 
 import com.minlia.cloud.holder.ContextHolder;
+import org.springframework.context.NoSuchMessageException;
 import org.springframework.context.i18n.LocaleContextHolder;
 
 import java.util.Locale;
@@ -8,15 +9,19 @@ import java.util.Locale;
 public class Lang {
 
     public static String get(String key, Object[] args, Locale locale) {
-        return ContextHolder.getContext().getMessage(key, args, locale);
+        try {
+            return ContextHolder.getContext().getMessage(key, args, locale);
+        } catch (NoSuchMessageException e) {
+            return key;
+        }
     }
 
     public static String get(String key, Object[] args) {
-        return ContextHolder.getContext().getMessage(key, args, LocaleContextHolder.getLocale());
+        return get(key, args, LocaleContextHolder.getLocale());
     }
 
     public static String get(String key) {
-        return ContextHolder.getContext().getMessage(key, new Object[]{}, LocaleContextHolder.getLocale());
+        return get(key, new Object[]{});
     }
-    
+
 }
