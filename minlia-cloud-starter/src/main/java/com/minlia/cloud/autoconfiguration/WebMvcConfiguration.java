8c3fda9d1612c8cfe9502cfc5be6c3d876c3a561
garen <191285052@qq.com>
2019-07-05 13:44:26 +0800

    Update at 20190705134426 by dev/garen

--- minlia-cloud-starter/src/main/java/com/minlia/cloud/autoconfiguration/WebMvcConfiguration.java
+++ minlia-cloud-starter/src/main/java/com/minlia/cloud/autoconfiguration/WebMvcConfiguration.java
@@ -1,20 +1,40 @@
 package com.minlia.cloud.autoconfiguration;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import com.minlia.cloud.resolver.UnderlineToCamelArgumentResolver;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.io.IOException;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Configuration
 @EnableWebMvc
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+    /**
+     * 默认日期时间格式
+     */
+    public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT);
 
     private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
             "classpath:/META-INF/resources/",
@@ -110,5 +130,123 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter implements Appl
 //    public ServletListenerRegistrationBean<RequestContextListener> requestContextListenerRegistration() {
 //        return new ServletListenerRegistrationBean<>(new RequestContextListener());
 //    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+        builder.serializationInclusion(JsonInclude.Include.NON_EMPTY);
+//        builder.propertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
+
+        // 启用美化打印
+        builder.indentOutput(true)
+//                .dateFormat(new SimpleDateFormat("yyyy-MM-dd"))
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)              // 禁用序列化日期为timestamps
+                .featuresToDisable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)           // 禁用遇到未知属性抛出异常
+                .featuresToDisable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)    // 禁用将日期调整到时间区域
+                .featuresToDisable(SerializationFeature.FAIL_ON_EMPTY_BEANS)                    // 禁用空对象序列化
+                .featuresToDisable(MapperFeature.DEFAULT_VIEW_INCLUSION)
+                .featuresToEnable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)    // 启用空字符传为null
+                .featuresToEnable(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES);               // 允许属性名称没有引号
+
+        builder.modules(javaTimeModule());
+        converters.add(new MappingJackson2HttpMessageConverter(builder.build()));
+//        converters.add(new MappingJackson2XmlHttpMessageConverter(builder.createXmlMapper(true).build()));
+    }
+
+    /**
+     * LocalDateTime系列序列化和反序列化模块，继承自jsr310，我们在这里修改了日期格式
+     *
+     * @return
+     */
+    private JavaTimeModule javaTimeModule() {
+//        ZoneOffset zoneOffset = ZoneOffset.of("+8");
+        ZoneOffset zoneOffset = ZoneOffset.ofHours(8);
+
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+//        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DATE_TIME_FORMATTER));
+//        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ISO_LOCAL_DATE));
+        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ISO_LOCAL_TIME));
+
+        javaTimeModule.addSerializer(LocalDateTime.class, new JsonSerializer<LocalDateTime>() {
+            @Override
+            public void serialize(LocalDateTime localDateTime, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+//                jsonGenerator.writeNumber(localDateTime.toEpochSecond(ZoneOffset.ofHours(8)));
+                jsonGenerator.writeNumber(localDateTime.toInstant(zoneOffset).toEpochMilli());
+            }
+        });
+
+        javaTimeModule.addSerializer(LocalDate.class, new JsonSerializer<LocalDate>() {
+            @Override
+            public void serialize(LocalDate localDate, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+                jsonGenerator.writeNumber(localDate.atStartOfDay().toInstant(zoneOffset).toEpochMilli());
+            }
+        });
+
+//        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DATE_TIME_FORMATTER));
+//        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ISO_LOCAL_DATE));
+//        javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ISO_LOCAL_TIME));
+
+        javaTimeModule.addDeserializer(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
+            @Override
+            public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+                if (NumberUtils.isDigits(jsonParser.getText())) {
+//                    return LocalDateTime.ofEpochSecond(jsonParser.getLongValue() / 1000, 0, ZoneOffset.ofHours(8));
+                    return LocalDateTime.ofInstant(Instant.ofEpochMilli(jsonParser.getLongValue()), zoneOffset);
+                } else {
+                    if (jsonParser.getText().contains("T")) {
+                        return LocalDateTime.parse(jsonParser.getText(), DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+                    } else {
+                        return LocalDateTime.parse(jsonParser.getText(), DATE_TIME_FORMATTER);
+                    }
+                }
+            }
+        });
+
+        javaTimeModule.addDeserializer(LocalDate.class, new JsonDeserializer<LocalDate>() {
+            @Override
+            public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+                if (NumberUtils.isDigits(jsonParser.getText())) {
+                    return LocalDateTime.ofInstant(Instant.ofEpochMilli(jsonParser.getLongValue()), zoneOffset).toLocalDate();
+                } else {
+                    return LocalDate.parse(jsonParser.getText());
+                }
+            }
+        });
+
+        javaTimeModule.addDeserializer(LocalTime.class, new JsonDeserializer<LocalTime>() {
+            @Override
+            public LocalTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+                if (NumberUtils.isDigits(jsonParser.getText())) {
+                    return LocalDateTime.ofInstant(Instant.ofEpochMilli(jsonParser.getLongValue()), zoneOffset).toLocalTime();
+                } else {
+                    return LocalTime.parse(jsonParser.getText());
+                }
+            }
+        });
+
+        //Date序列化和反序列化
+//        javaTimeModule.addSerializer(Date.class, new JsonSerializer<Date>() {
+//            @Override
+//            public void serialize(Date date, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+//                SimpleDateFormat formatter = new SimpleDateFormat(DEFAULT_DATE_TIME_FORMAT);
+//                String formattedDate = formatter.format(date);
+//                jsonGenerator.writeString(formattedDate);
+//            }
+//        });
+//        javaTimeModule.addDeserializer(Date.class, new JsonDeserializer<Date>() {
+//            @Override
+//            public Date deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+//                SimpleDateFormat format = new SimpleDateFormat(DEFAULT_DATE_TIME_FORMAT);
+//                String date = jsonParser.getText();
+//                try {
+//                    return format.parse(date);
+//                } catch (ParseException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+//        });
+        return javaTimeModule;
+    }
 
 }
