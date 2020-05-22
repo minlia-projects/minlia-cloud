a19e5ae574262251333e29aa74669434fc978ed9
garen <191285052@qq.com>
2020-05-22 17:40:45 +0800

    update

--- minlia-cloud-starter/src/main/java/com/minlia/cloud/exception/handler/RestErrorController.java
+++ minlia-cloud-starter/src/main/java/com/minlia/cloud/exception/handler/RestErrorController.java
@@ -11,8 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.context.request.ServletWebRequest;
 
 import javax.servlet.http.HttpServletRequest;
