b719e2095ff4bb9a55bb69280f00bbe2d964caaf
garen <191285052@qq.com>
2020-05-25 17:56:02 +0800

    update

---
+++ minlia-cloud-starter/src/main/java/com/minlia/cloud/Application.java
@@ -0,0 +1,13 @@
+package com.minlia.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.minlia.cloud.Application.class, args);
+    }
+
+}
