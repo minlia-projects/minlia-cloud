6cde36453ab3495529dea7f283391e265fe9a908
will <will@minlia.com>
2020-06-23 17:16:25 +0800

    Updated with list body

---
+++ minlia-cloud-starter/src/main/java/com/minlia/cloud/body/WithListBody.java
@@ -0,0 +1,20 @@
+package com.minlia.cloud.body;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+/**
+ * Created by will on 6/23/20.
+ */
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WithListBody<T> implements Body {
+    private List<T> items;
+
+}
