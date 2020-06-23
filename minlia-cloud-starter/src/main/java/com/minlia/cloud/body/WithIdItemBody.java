89addafcaaf622769d81dad796778b7267f62d9b
will <will@minlia.com>
2020-06-23 17:16:22 +0800

    Updated with list body

--- minlia-cloud-starter/src/main/java/com/minlia/cloud/body/WithIdItemBody.java
+++ minlia-cloud-starter/src/main/java/com/minlia/cloud/body/WithIdItemBody.java
@@ -1,6 +1,9 @@
 package com.minlia.cloud.body;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
@@ -8,6 +11,10 @@ import java.util.List;
  * Created by will on 6/20/17.
  */
 @Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WithIdItemBody implements Body {
     private List<WithIdBody> items;
+
 }
--- minlia-cloud-starter/src/main/java/com/minlia/cloud/body/WithNameRequestBody.java
+++ minlia-cloud-starter/src/main/java/com/minlia/cloud/body/WithNameRequestBody.java
@@ -4,13 +4,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 /**
  * Created by will on 6/20/17.
  */
 @ApiModel(value = "带名称的请求体")
 @Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WithNameRequestBody implements Body {
     @ApiModelProperty(value = "名称")
     @JsonProperty
